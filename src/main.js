d37ccd177ec7a8ce212cf774435a6b328e0c019f
Lebohang <lebohangmokhwibitjane@gmail.com>
2024-10-03 00:48:32 +0200

    can give command to change ehat is on screen

--- src/main.js
+++ src/main.js
@@ -9,21 +9,35 @@ const commands = [
 
 let input=document.getElementById("input");
 let output=document.getElementById("output");
+let path=document.getElementById("path");
+let prompt=document.getElementById("prompt");
+let line=document.getElementById("carrot");
 let cmd = "";
 
-function command(cmd){
+document.getElementById("input").addEventListener("keypress", e => {
+    if (e.key === "Enter" && !e.shiftKey) {
+        e.preventDefault();
+        execute()
+    }
+});
+
+function command(){
   if(commands.includes(cmd.trim().toUpperCase())){
     console.log("Command found in array");
   }else{
     console.log("Invalid command");
   }
 }
+//
+// function update(){
+//   if(input.value.charAt(input.value.length-1) == "\n"){
+//     console.log("end of line");
+//     command();
+//   }
+//   cmd = input.value
+//   // output.innerHTML = cmd;
+// }
 
-function update(){
-  if(input.value.charAt(input.value.length-1) == "\n"){
-    console.log("end of line");
-    command(cmd);
-  }
-  cmd = input.value
-  output.innerHTML = cmd;
+function execute(){
+  carrot.innerHTML="Fucked"
 }
