8f2d2d94058a336164a40595da222760b6bc3b97
Lebohang <lebohangmokhwibitjane@gmail.com>
2025-01-22 06:58:17 +0200

    commands pointer to function dictinary

--- src/command.js
+++ src/command.js
@@ -1,7 +1,44 @@
+import print from "./command_list.js"
 
-export default function execute(command){
+let commands = {
+    "set":add_command,
+    "print":print
+};
+let vars = {};
+
+
+export default function execute(str_cmd){
     return new Promise((resolve)=>{
+	const command = str_cmd.split(" ")
+	// if(command.length>1){
+	//     window[command[0]]();
+	// }else{
+	//     window.[command[0]](command.slice(1, command.length-1));
+	// }
+	get_commands()[command[0]](window.document,command)
 	console.log(`Executed: ${command}`);
 	resolve();
     });
 }
+
+export function get_commands(){return commands;}
+
+export function add_command(name, reference){
+    let override = false;
+    if(name in commands){
+	// TODO: get user input
+	// check if they want to overide exsisting reference
+    }else{
+	override=true;
+    }
+
+    if(override){
+	commands[name]=reference;
+    }
+}
+
+export function remove_command(name){
+    delete commands[name];
+}
+
+
---
+++ src/command_list.js
@@ -0,0 +1,5 @@
+
+export default function print(window, value){
+    console.log(value)
+    console.log(window);
+}
